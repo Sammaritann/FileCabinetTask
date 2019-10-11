479f9d57e98322c83b670f4524ed96242d7a90fd
pasha.frigin <pasha.frigin@gmail.com>
2019-10-11 14:14:45 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -9,6 +9,46 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short department, decimal salary, char clas)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException($"{nameof(firstName)} must not be null or contain only spaces");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException($"{nameof(lastName)} must not be null or contain only spaces");
+            }
+
+            if ((firstName.Length < 2) || (firstName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(firstName)} length should be between 2 and 60");
+            }
+
+            if ((lastName.Length < 2) || (lastName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(lastName)} length should be between 2 and 60");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} shoud be between  01-Jan-1950 and now");
+            }
+
+            if (department <= 0)
+            {
+                throw new ArgumentException($"{nameof(department)} should be more than zero");
+            }
+
+            if (salary <= 0)
+            {
+                throw new ArgumentException($"{nameof(salary)} must be more than zero");
+            }
+
+            if (clas < 'A' || clas > 'Z')
+            {
+                throw new ArgumentException($"{nameof(clas)} should be between A and Z");
+            }
+
             FileCabinetRecord record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
