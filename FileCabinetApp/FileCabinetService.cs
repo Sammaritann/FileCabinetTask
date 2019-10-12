2df3b218c767068f3ca766e16ebee5ecdc58d83e
pasha.frigin <pasha.frigin@gmail.com>
2019-10-12 19:24:48 +0300

    Improve find firstname performance with dictionary.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -7,6 +7,7 @@ namespace FileCabinetApp
     public class FileCabinetService
     {
         private readonly List<FileCabinetRecord> list = new List<FileCabinetRecord>();
+        private readonly Dictionary<string, List<FileCabinetRecord>> firstNameDictionary = new Dictionary<string, List<FileCabinetRecord>>();
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short department, decimal salary, char clas)
         {
@@ -63,6 +64,12 @@ namespace FileCabinetApp
 
             this.list.Add(record);
 
+            if (!this.firstNameDictionary.ContainsKey(firstName.ToUpperInvariant()))
+            {
+                this.firstNameDictionary.Add(firstName.ToUpperInvariant(), new List<FileCabinetRecord>());
+            }
+
+            this.firstNameDictionary[firstName.ToUpperInvariant()].Add(record);
             return record.Id;
         }
 
@@ -85,6 +92,58 @@ namespace FileCabinetApp
                 throw new ArgumentException($"wrong {nameof(id)}");
             }
 
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException($"{nameof(firstName)} must not be null or contain only spaces");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException($"{nameof(lastName)} must not be null or contain only spaces");
+            }
+
+            if ((firstName.Length < 2) || (firstName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(firstName)} length should be between 2 and 60");
+            }
+
+            if ((lastName.Length < 2) || (lastName.Length > 60))
+            {
+                throw new ArgumentException($"{nameof(lastName)} length should be between 2 and 60");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException($"{nameof(dateOfBirth)} shoud be between  01-Jan-1950 and now");
+            }
+
+            if (department <= 0)
+            {
+                throw new ArgumentException($"{nameof(department)} should be more than zero");
+            }
+
+            if (salary <= 0)
+            {
+                throw new ArgumentException($"{nameof(salary)} must be more than zero");
+            }
+
+            if (clas < 'A' || clas > 'Z')
+            {
+                throw new ArgumentException($"{nameof(clas)} should be between A and Z");
+            }
+
+            if (record.FirstName.ToUpperInvariant() != firstName.ToUpperInvariant())
+            {
+                this.firstNameDictionary[record.FirstName.ToUpperInvariant()].Remove(record);
+
+                if (!this.firstNameDictionary.ContainsKey(firstName.ToUpperInvariant()))
+                {
+                    this.firstNameDictionary.Add(firstName.ToUpperInvariant(), new List<FileCabinetRecord>());
+                }
+
+                this.firstNameDictionary[firstName.ToUpperInvariant()].Add(record);
+            }
+
             record.FirstName = firstName;
             record.LastName = lastName;
             record.DateOfBirth = dateOfBirth;
@@ -95,7 +154,12 @@ namespace FileCabinetApp
 
         public FileCabinetRecord[] FindByFirstName(string firstName)
         {
-            return this.list.FindAll((x) => x.FirstName.ToUpperInvariant() == firstName.ToUpperInvariant()).ToArray();
+            if (firstName is null)
+            {
+                throw new ArgumentNullException($"{nameof(firstName)} must not be null");
+            }
+
+            return this.firstNameDictionary[firstName.ToUpperInvariant()].ToArray();
         }
 
         public FileCabinetRecord[] FindByLastName(string lastName)
