a936d46efe2ddc3d219004d11bd40621f78ec1df
pasha.frigin <pasha.frigin@gmail.com>
2019-10-27 13:28:48 +0300

    Apply Extract Method  for records validator

--- FileCabinetApp/Validators/RecordValidator/CustomValidator.cs
+++ FileCabinetApp/Validators/RecordValidator/CustomValidator.cs
@@ -33,19 +33,32 @@ namespace FileCabinetApp.Validators
                 throw new ArgumentNullException($"{nameof(recordParams)} must not be null");
             }
 
+            this.ValidateFirstName(recordParams);
+            this.ValidateLastName(recordParams);
+            this.ValidateDateOfBirth(recordParams);
+            this.ValidateDepartment(recordParams);
+            this.ValidateSalary(recordParams);
+            this.ValidateClass(recordParams);
+        }
+
+        private void ValidateFirstName(RecordParams recordParams)
+        {
             if (string.IsNullOrWhiteSpace(recordParams.FirstName))
             {
                 throw new ArgumentNullException($"{nameof(recordParams.FirstName)} must not be null or contain only spaces");
             }
 
-            if (string.IsNullOrWhiteSpace(recordParams.LastName))
-            {
-                throw new ArgumentNullException($"{nameof(recordParams.LastName)} must not be null or contain only spaces");
-            }
-
             if ((recordParams.FirstName.Length < MinNameLength) || (recordParams.FirstName.Length > MaxNameLength))
             {
                 throw new ArgumentException($"{nameof(recordParams.FirstName)} length should be between 2 and 60");
+            }
+        }
+
+        private void ValidateLastName(RecordParams recordParams)
+        {
+            if (string.IsNullOrWhiteSpace(recordParams.LastName))
+            {
+                throw new ArgumentNullException($"{nameof(recordParams.LastName)} must not be null or contain only spaces");
             }
 
             if ((recordParams.LastName.Length < MinNameLength) || (recordParams.LastName.Length > MaxNameLength))
@@ -53,25 +66,42 @@ namespace FileCabinetApp.Validators
                 throw new ArgumentException($"{nameof(recordParams.LastName)} length should be between 2 and 60");
             }
 
+        }
+
+        private void ValidateDateOfBirth(RecordParams recordParams)
+        {
             if (recordParams.DateOfBirth < DateMin || recordParams.DateOfBirth > DateTime.Now)
             {
                 throw new ArgumentException($"{nameof(recordParams.DateOfBirth)} shoud be between  01-Jan-1950 and now");
             }
 
+        }
+
+        private void ValidateDepartment(RecordParams recordParams)
+        {
             if (recordParams.Department <= 0)
             {
                 throw new ArgumentException($"{nameof(recordParams.Department)} should be more than zero");
             }
 
+        }
+
+        private void ValidateSalary(RecordParams recordParams)
+        {
             if (recordParams.Salary <= 0)
             {
                 throw new ArgumentException($"{nameof(recordParams.Salary)} must be more than zero");
             }
 
+        }
+
+        private void ValidateClass(RecordParams recordParams)
+        {
             if (recordParams.Class < MinClass || recordParams.Class > MaxClass)
             {
                 throw new ArgumentException($"{nameof(recordParams.Class)} should be between A and Z");
             }
+
         }
     }
 }
